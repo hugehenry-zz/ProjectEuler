adabfca7429cb772b05e41e0f0d8ca25c111c13a
hugeHenry <ma.dah@outlook.com>
2018-02-26 11:47:05 +0100

    Complete

--- Problems_1-100/P1_multof3and5.py
+++ Problems_1-100/P1_multof3and5.py
@@ -1,5 +1,5 @@
 def mult35(upp_lim):
-	# Return the sum all the multiples of 3 and 5 from 0 to upp_lim
+	# Return the sum all the multiples of 3 and 5 from 0 to upp_lim.
 	tot_sum = 0
 	for i in range(upp_lim):
 		if i % 3 == 0 or i % 5 == 0:
